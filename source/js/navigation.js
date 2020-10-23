715b27a41f7198df11ae86d94ebe67683ec2c069
Richie <mari4orl@mail.ru>
2020-10-23 17:31:02 +0300

    Правки2

--- source/js/navigation.js
+++ source/js/navigation.js
@@ -1,9 +1,10 @@
+'use strict'
 var navigation = document.querySelector('.navigation');
 var navToggle = document.querySelector('.navigation__toggle');
 
 navigation.classList.remove('navigation--nojs');
 
-navToggle.addEventListener('click', function() {
+navToggle.addEventListener('click', function () {
   if (navigation.classList.contains('navigation--closed')) {
     navigation.classList.remove('navigation--closed');
     navigation.classList.add('navigation--opened');
